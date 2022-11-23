e83520453fa68f55a0c0768711cb933dfa35f610
Erik-Bizderi <erik.bizderi@gmail.com>
2022-11-23 13:58:35 +0100

    Add project files.

---
+++ JokeModels/Joke.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace softtech2_9gyak_XZ6IVT.JokeModels;
+
+public partial class Joke
+{
+    public int JokeSk { get; set; }
+
+    public string? JokeText { get; set; }
+
+    public int UpVotes { get; set; }
+
+    public int DownVotes { get; set; }
+}
